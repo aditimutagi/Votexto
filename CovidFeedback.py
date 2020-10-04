28191e617cc5cab94fe9b0500017d0b2ae2b0ed3
aditimutagi <68683431+aditimutagi@users.noreply.github.com>
2020-10-04 12:59:40 -0500

    Update CovidFeedback.py

--- CovidFeedback.py
+++ CovidFeedback.py
@@ -14,6 +14,6 @@ if (covidFeedback == 'Y'):
     sd = input("Were social distancing measures strictly regulated and followed? Y or N\n")
     masks = input("To what extent were masks worn regularly and properly (that being, covering the nose and mouth)? Please enter L for low mask usage, M for moderate, and H for high\n")
     santization = input("Were places and objects of common contact sanitized after each use? Y or N\n")
-    wait = input("About how long was your wattime?")
+    wait = input("About how long was your wattime?\n")
     
 print("Thank you for using Votexto! We greatly appreciate your responses as it will help us better inform other members of your community during this Election Day! We hope to see you at the polls during the next election!\n")
